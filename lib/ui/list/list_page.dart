c85158c1b80748ad16777ebdacb27fe196639bbe
jBosak98 <jbosak98@gmail.com>
2019-09-14 16:14:21 +0200

    simple list

--- lib/ui/list/list_page.dart
+++ lib/ui/list/list_page.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter1/common/bloc/stack_questions_bloc.dart';
 import 'package:flutter1/common/model/stack_questions/stack_questions.dart';
-import 'package:flutter1/common/rx/rx_response.dart';
-import 'package:flutter1/common/rx/rx_status.dart';
 import 'package:flutter1/ui/list/list_item.dart';
 
 class ListPage extends StatefulWidget {
-  ListPage(this.stackQuestionsBloc, {Key key, this.title}): super(key: key);
+  ListPage(this.stackQuestionsBloc, {Key key, this.title}) : super(key: key);
 
   final StackQuestionsBloc stackQuestionsBloc;
   final String title;
@@ -15,102 +13,52 @@ class ListPage extends StatefulWidget {
   State<StatefulWidget> createState() => _ListPageState();
 }
 
-class _ListPageState extends State<ListPage>{
-
-  bool _activeSearchBar;
-
-  @override
-  void initState() {
-    super.initState();
-    _activeSearchBar = false;
-    widget.stackQuestionsBloc.requestStackQuestions(forceUpdate: false);
-  }
-
+class _ListPageState extends State<ListPage> {
   Future<void> _refreshStackQuestions() async {
     widget.stackQuestionsBloc.requestStackQuestions(forceUpdate: true);
   }
 
   @override
   Widget build(BuildContext context) {
+    var list = ["one", "two", "three", "four"];
     return Scaffold(
-      appBar: _appBar(),
-      body: RefreshIndicator(
-        child: StreamBuilder(
-          stream: widget.stackQuestionsBloc.questionStream,
-            builder: (context, AsyncSnapshot<RxResponse<StackQuestions>> snapshot) {
-              if (snapshot.hasData && snapshot.data.status == RxStatus.SUCCESS) {
-                return _listViewBuilder(snapshot.data.data);
-              } else if (snapshot.hasError || snapshot.data?.status == RxStatus.ERROR) {
-                return _fetchErrorMsg(snapshot.error);
-              }
-              return Center(child: CircularProgressIndicator());
-            }),
-        onRefresh: _refreshStackQuestions,
-      ),
-    );
+        appBar: AppBar(title: Text('Your friends')),
+        body: Center(
+            child: ListView(
+                children: list
+                    .map((item) => new Card(
+                            child: Row(children: <Widget>[
+                          Text(item),
+                          Align(
+                              alignment: Alignment.centerRight,
+                              child: Text("xd"))
+                        ])))
+                    .toList())));
   }
 
-  Widget _appBar() {
-    return !_activeSearchBar ?
-    AppBar(
-      title: Text(widget.stackQuestionsBloc.lastSearch() != null
-          ? "Tag: " + widget.stackQuestionsBloc.lastSearch()
-          : widget.title),
-      actions: <Widget>[
-        IconButton(
-          icon: Icon(Icons.search),
-          onPressed: () => setState(() => _activeSearchBar = true),
-        ),
-      ],
-    ) : AppBar(
-      leading: Icon(Icons.search),
-      title: TextFormField(
-          initialValue: widget.stackQuestionsBloc.lastSearch(),
-          decoration: InputDecoration(
-            hintText: "Search stack question topic",
-          ),
-          autofocus: true,
-          onFieldSubmitted: (input) {
-            widget.stackQuestionsBloc.requestStackQuestions(
-                search: input, forceUpdate: true);
-            setState(() => _activeSearchBar = false);
-          }),
-      actions: <Widget>[
-        IconButton(
-          icon: Icon(Icons.close),
-          onPressed: () => setState(() => _activeSearchBar = false),
-        ),
-      ],
-    );
-  }
+//      new Column(children: list.map((item) => new Card(child: Text(item))).toList());
 
-  Widget _listViewBuilder(StackQuestions response){
+  Widget _listViewBuilder(StackQuestions response) {
     return ListView.separated(
         itemCount: response.items.length,
         padding: EdgeInsets.only(top: 8.0, bottom: 8.0),
         itemBuilder: (BuildContext context, int index) =>
             ListItem(index: index, model: response.items[index]),
-        separatorBuilder: (BuildContext context,
-            int index) => const Divider());
+        separatorBuilder: (BuildContext context, int index) => const Divider());
   }
 
   Widget _fetchErrorMsg(Exception e) {
-    return Column(children: <Widget>[
-      Padding(
-        padding: EdgeInsets.all(12.0),
-        child: Expanded(
-          child: Card(
-            child: Text("Unable to fetch: " + e.toString()),
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.all(12.0),
+          child: Expanded(
+            child: Card(
+              child: Text("Unable to fetch: " + e.toString()),
+            ),
           ),
         ),
-      ),
-    ],
+      ],
     );
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    widget.stackQuestionsBloc.dispose();
   }
 }
